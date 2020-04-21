fd8f15ba028dad8f49b9819a88594e8f7a45ebf7
cbarban <corentin.barban@sfr.fr>
2020-04-21 16:51:01 +0200

    Structure composite

---
+++ src/main/java/com/barban/corentin/m2/miagesousleau/miagesousleaucomposite/MiagesousleaucompositeApplication.java
@@ -0,0 +1,13 @@
+package com.barban.corentin.m2.miagesousleau.miagesousleaucomposite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiagesousleaucompositeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiagesousleaucompositeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barban/corentin/m2/miagesousleau/miagesousleaucomposite/MiagesousleaucompositeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barban.corentin.m2.miagesousleau.miagesousleaucomposite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiagesousleaucompositeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
